a562b73088679d0a4fb9841b39b38d97fbd10e44
kturki85@gmail.com <kturki85@gmail.com@f23a729a-37c5-9f90-ca8d-480bc7a2f78d>
2011-11-09 22:42:43 +0000

    creation des MessagesItems obligatoire pour la creation d'un message

--- Elastic/MessagesLibrary/Message.cs
+++ Elastic/MessagesLibrary/Message.cs
@@ -5,9 +5,25 @@ using System.Text;
 
 namespace MessagesLibrary
 {
+
     public class Message
     {
         private int count;
         private List<MessageItem> lsItems;
+
+        public Message(String src,String tgt,String op,String stp,int nbParams)
+        {
+            lsItems = new List<MessageItem>();
+            MessageItem source = new MessageItem("string", "utf-8", "source", true, src);
+            MessageItem target = new MessageItem("string", "utf-8", "target", true, tgt);
+            MessageItem operation = new MessageItem("string", "utf-8", "operation", true, op);
+            MessageItem stamp = new MessageItem("string", "utf-8", "stamp", true, stp);
+            MessageItem nbrparams = new MessageItem("int", "binaire", "paramCount", true, nbParams);
+            lsItems.Add(source);
+            lsItems.Add(target);
+            lsItems.Add(operation);
+            lsItems.Add(stamp);
+            lsItems.Add(nbrparams);
+        }
     }
 }
