a6f15cbf2e66f0448ecbe297c7473ad12a87285b
longbien86@gmail.com <longbien86@gmail.com@f23a729a-37c5-9f90-ca8d-480bc7a2f78d>
2011-11-09 19:30:16 +0000

    add message item class

---
+++ Elastic/MessagesLibrary/MessageItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessagesLibrary
+{
+     public class MessageItem
+    {
+         public int Count {get;set;}
+         public string Type { get; set; }
+         public string Codage { get; set; }
+         public string Key { get; set; }
+         public bool IsMandatory { get; set; }
+
+         
+    }
+}
