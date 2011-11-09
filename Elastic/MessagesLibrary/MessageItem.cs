c0e7848153d013bd036ea3af05e62a95b30a61d6
kturki85@gmail.com <kturki85@gmail.com@f23a729a-37c5-9f90-ca8d-480bc7a2f78d>
2011-11-09 22:41:47 +0000

    ajout d'une propriete Object qui represente la valeur/semantique de l'itemMessage, vu que le Message item a un champ type le service pourra caster avec le bon type la propriete object.

--- Elastic/MessagesLibrary/MessageItem.cs
+++ Elastic/MessagesLibrary/MessageItem.cs
@@ -7,13 +7,16 @@ namespace MessagesLibrary
 {
      public class MessageItem
     {
+         int count { get; set; }
          public string Type { get; set; }
          public string Codage { get; set; }
          public string Key { get; set; }
          public bool IsMandatory { get; set; }
+         public Object value { get; set; }
 
-         public MessageItem(string Type, string Codage, string Key, bool IsMandatory) 
+         public MessageItem(string Type, string Codage, string Key, bool IsMandatory, Object value) 
          {
+             this.count = 0;
              this.Type = Type;
              this.Codage = Codage;
              this.Key = Key;
