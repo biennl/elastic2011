e6735601558a7c1132d1bbcd590206c6cc9bddfb
kturki85@gmail.com <kturki85@gmail.com@f23a729a-37c5-9f90-ca8d-480bc7a2f78d>
2011-11-09 23:07:11 +0000

    ajout methode addParametre : ajoute un parametre a a liste qui contient le message

--- trunk/Elastic/MessagesLibrary/Message.cs
+++ trunk/Elastic/MessagesLibrary/Message.cs
@@ -11,19 +11,27 @@ namespace MessagesLibrary
         private int count;
         private List<MessageItem> lsItems;
 
-        public Message(String src,String tgt,String op,String stp,int nbParams)
+        public Message(String src,String tgt,String op,String stp,int nbParams,String descr)
         {
             lsItems = new List<MessageItem>();
-            MessageItem source = new MessageItem("string", "utf-8", "source", true, src);
-            MessageItem target = new MessageItem("string", "utf-8", "target", true, tgt);
-            MessageItem operation = new MessageItem("string", "utf-8", "operation", true, op);
-            MessageItem stamp = new MessageItem("string", "utf-8", "stamp", true, stp);
-            MessageItem nbrparams = new MessageItem("int", "binaire", "paramCount", true, nbParams);
+            //each describing string "descr" are null
+            //a minimum message contains this 5 MessageIndex
+            MessageItem source = new MessageItem("string", "utf-8", "source", true, src,"");
+            MessageItem target = new MessageItem("string", "utf-8", "target", true, tgt,"");
+            MessageItem operation = new MessageItem("string", "utf-8", "operation", true, op,"");
+            MessageItem stamp = new MessageItem("string", "utf-8", "stamp", true, stp,"");
+            MessageItem nbrparams = new MessageItem("int", "binaire", "paramCount", true, nbParams,"");
             lsItems.Add(source);
             lsItems.Add(target);
             lsItems.Add(operation);
             lsItems.Add(stamp);
             lsItems.Add(nbrparams);
         }
+
+        //add a parameter to the list 
+        public void addParams(String type, String format, String key, object value,String descr)
+        {
+            lsItems.Add(new MessageItem(type,format,key,false,value,descr));
+        } 
     }
 }
