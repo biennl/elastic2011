e5e8ebe05aaf8ae0815ccba6546629695ff709a6
longbien86@gmail.com <longbien86@gmail.com@f23a729a-37c5-9f90-ca8d-480bc7a2f78d>
2011-11-10 16:59:49 +0000

    - Add Encoding Library: create methods to encode and decode messages. - Modify a bit Message class.

---
+++ Elastic/ConsoleTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EncodingLibrary;
+using MessagesLibrary;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MsgEncoding encoding = new MsgEncoding();
+            Message msg = new Message();
+            msg.Count = 899;
+            msg.Source = "Machine A";
+            msg.Target = "Machine B";
+            msg.Operation = "Echo";
+            msg.Stamp = "Service Temp";
+            msg.ParamCount = 3;
+
+            byte[] count = encoding.Encode(msg);
+
+            Console.WriteLine("Avant: "+msg.ToString());
+
+            Message returnMsg = encoding.Decode(count);
+
+            Console.WriteLine("Apres: " + returnMsg.ToString());
+        }
+    }
+}
