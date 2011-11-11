1889849863992fd163aba7dd80ca55f4f37e2cdc
imouctar@gmail.com <imouctar@gmail.com@f23a729a-37c5-9f90-ca8d-480bc7a2f78d>
2011-11-11 14:16:08 +0000

    creation d'une nvlle class: CatalogInfos; elle sert de structure qui represente les infos d'un service

---
+++ Elastic/CatalogService/ServiceInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CatalogService
+{
+    class ServiceInfo
+    {
+        public string Service { get; }
+        public string Title   { set; get; }
+        public string Address { set; get; }
+        public string Port    { set; get; }
+
+        public ServiceInfo(string title, string address, string port) 
+        {
+            this.Title   = title;
+            this.Address = address;
+            this.Port = port;
+ 
+        }
+
+    }
+}
