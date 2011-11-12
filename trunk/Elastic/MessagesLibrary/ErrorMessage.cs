b2b4f1c2ea1c663664b8eb11507d585d41d0049f
kturki85@gmail.com <kturki85@gmail.com@f23a729a-37c5-9f90-ca8d-480bc7a2f78d>
2011-11-12 15:08:09 +0000

    message d'erreur rajouter

---
+++ trunk/Elastic/MessagesLibrary/ErrorMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessagesLibrary
+{
+    class ErrorMessage : Message
+    {
+        public String errorMessage { get; set; }
+
+        public ErrorMessage(String errorMessage,String source,String target,string stamp)
+        {
+            this.errorMessage = errorMessage;
+            this.Source = source;
+            this.Target=target;
+            this.Stamp = stamp;
+            this.Operation = "diagnostique";
+            this.ParamCount = 1;
+        }
+    }
+}
